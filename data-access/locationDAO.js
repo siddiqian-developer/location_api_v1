616f4f37869e5e741178d181af13e40ca8d717e7
lowkeystone <54378250+lowkeystone@users.noreply.github.com>
2021-09-07 12:11:56 -0400

    Update locationDAO.js

--- data-access/locationDAO.js
+++ data-access/locationDAO.js
@@ -21,4 +21,5 @@ export default class LocationDAO {
 
   // Write database methods here, call these methods in location.controller.js file :)
   // Feel free to simplify the location object in the database, especially if there is a performance gain!
+  // Your db username will have admin privileges, if not, please send me a message. Thank you!
 }
