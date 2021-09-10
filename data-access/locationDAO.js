fdfe9c281cb16ffa5588355d07d227aadc568d8d
Bryant Mejia <a999999999@Bryants-MacBook-Pro.local>
2021-09-09 23:39:30 -0400

    updated data-access method

--- data-access/locationDAO.js
+++ data-access/locationDAO.js
@@ -7,10 +7,8 @@ export default class LocationDAO {
       return
     }
     try {
-
       //define database collections here
-      vendorsColl = conn.db(dbName).collection("vendors")
-      
+      vendorsColl = await conn.db(dbName).collection("vendors")
     } catch (e) {
       console.error(`Unable to establish collection handles in LocationDAO: ${e}`)
     }
@@ -18,7 +16,15 @@ export default class LocationDAO {
   /*====================
   Location Access Object Methods
   ====================*/
-
+  static async getVendors() {
+    try {
+      console.log(vendorsColl)
+      const vendors = await vendorsColl.find().toArray()
+      return vendors
+    } catch (e) {
+      return console.error(`error: ${e}`)
+    }
+  }
   // Write database methods here, call these methods in location.controller.js file :)
   // Feel free to simplify the location object in the database, especially if there is a performance gain!
   // Your db username will have admin privileges, if not, please send me a message. Thank you!
